ba076228c9b8bf3aaa436c6e2426461963d65187
何威 <824056342@qq.com>
2021-01-19 17:03:32 +0800

    暂存修改

--- app/controller/Jpush.php
+++ app/controller/Jpush.php
@@ -2,17 +2,17 @@
 namespace app\controller;
 
 use app\BaseController;
-use app\lib\jpush\Jpush;
+use app\lib\jpush\Jpush as JpushLib;
 use app\lib\jpush\JpushStatic;
 
-class Index extends BaseController
+class Jpush extends BaseController
 {
-    public function index()
+    public function push()
     {
         $regId = ['100d855909f5342e530'];
         var_dump(JpushStatic::push($regId,[JpushStatic::PLATFORM_ANDROID],"欢迎来到犀鸟公考","犀鸟通知"));
 
-        $jpush = new Jpush();
+        $jpush = new JpushLib();
         var_dump($jpush->registrationIdAdd($regId)->notificationAndroid("欢迎来到犀鸟公考2","犀鸟通知")->push());
     }
 }
